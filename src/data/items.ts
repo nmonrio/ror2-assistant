794426595ea5357d07417b53996c24a2fd22927b
Nerea <nereamonrio10@gmail.com>
2026-09-07 00:49:29 +0200

    Included imgs for trial items.

--- src/data/items.ts
+++ src/data/items.ts
@@ -18,6 +18,7 @@ export interface Item {
   name: string;
   tier: ItemTier;
   dlc: DLC;
+  image: string;
 }
 
 export const items: Item[] = [
@@ -26,35 +27,41 @@ export const items: Item[] = [
     name: "Soldier's Syringe",
     tier: "common",
     dlc: "base",
+    image: "/items/soldiers-syringe.png",
   },
   {
     id: "lens-makers-glasses",
     name: "Lens-Maker's Glasses",
     tier: "common",
     dlc: "base",
+    image: "/items/lens-maker-glasses.png",
   },
   {
     id: "tougher-times",
     name: "Tougher Times",
     tier: "common",
     dlc: "base",
+    image: "/items/tougher-times.png",
   },
   {
     id: "ukulele",
     name: "Ukulele",
     tier: "uncommon",
     dlc: "base",
+    image: "/items/ukulele.png",
   },
   {
     id: "atg-missile",
     name: "AtG Missile Mk. 1",
     tier: "uncommon",
     dlc: "base",
+    image: "/items/atg-missile-mk1.png",
   },
   {
     id: "57-leaf-clover",
     name: "57 Leaf Clover",
     tier: "legendary",
     dlc: "base",
+    image: "/items/57-leaf-clover.png",
   },
 ];
